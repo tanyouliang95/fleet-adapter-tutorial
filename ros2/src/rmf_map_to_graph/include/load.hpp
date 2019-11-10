22695724134ecb9bbb27a8eca88be223b6e7e203
Boon Han <cnboonhan94@gmail.com>
2019-11-10 20:20:06 +0800

    add rmf_map_to_graph package

---
+++ ros2/src/rmf_map_to_graph/include/load.hpp
@@ -0,0 +1,10 @@
+#ifndef RMF_MAP_TO_GRAPH__LOAD_HPP
+#define RMF_MAP_TO_GRAPH__LOAD_HPP
+
+namespace rmf_map_to_graph {
+
+
+
+} // namespace rmf_map_to_graph
+
+#endif // RMF_MAP_TO_GRAPH__LOAD_HPP
