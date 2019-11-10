1a747c04ddb045d34105b5895a3d886f13037b34
Boon Han <cnboonhan94@gmail.com>
2019-11-10 23:27:59 +0800

    minor update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Parse YAML to Graph
-This section will focus on parsing the `map.yaml` file, which is the output of `rmf-editor`, into the `Graph` data structure which is found in RMF. Libraries have already been written to do this task. This section is meant to give understanding of how this process works.
+This section will focus on parsing the `maze.yaml` file, which is the output of `rmf-editor`, into the `Graph` data structure which is found in RMF. Libraries have already been written to do this task. This section is meant to give understanding of how this process works.
+
+## Mechanics
+All data is stored in the `maze.yaml` file. In particular, the information under `vertices` and `lanes`. In `rmf_map_to_graph`, we iterate through each of the entries. For each entry, the `load_graph` function will add the corresponding `vertex` or `lane` into a Graph. This graph representation is then used by the RMF Planner to decide the best path from one vertex to another.
+
+Note that this is a high level path planning. For actual robot movement between two nodes in terms motor controls such as `cmd_vel`, we should expect that the robots own navstack will handle it.
--- ros2/src/rmf_map_to_graph/src/load_graph.cpp
+++ ros2/src/rmf_map_to_graph/src/load_graph.cpp
@@ -26,12 +26,18 @@ rmf_traffic::agv::Graph load(std::string test_map_name, std::string path)
     for (YAML::const_iterator it = y_vtx.begin(); it != y_vtx.end(); ++it)
     {
         std::string vertex_name = (*it)[3].as<std::string>();
+        int x_pos = (*it)[0].as<int>();
+        int y_pos = (*it)[1].as<int>();
+        int z_pos = (*it)[2].as<int>();
         std::cout << "Vertex " << vertex_name
                   << " is at position "
-                  << "(" << (*it)[0].as<int>()
-                  << "," << (*it)[1].as<int>() << "," << (*it)[2].as<int>()
+                  << "(" << x_pos
+                  << "," << y_pos 
+                  << "," << z_pos
                   << ")\n";
-                  
+        
+        graph.add_waypoint(test_map_name, {x_pos, y_pos});
+
         // It seems graph indexes nodes by order of addition into the graph.
         // We should probably give a mapping of "vertex name -> graph index".
         vertex_name_id_map[vertex_name] = vertex_id; // consider returning this
@@ -43,20 +49,28 @@ rmf_traffic::agv::Graph load(std::string test_map_name, std::string path)
 
     for (YAML::const_iterator it = y_lanes.begin(); it != y_lanes.end(); ++it)
     {
-        std::string origin_vertex_name = (*vertex_id_name_map.find((*it)[0].as<int>())).second;
-        std::string tail_vertex_name = (*vertex_id_name_map.find((*it)[1].as<int>())).second;
+        int origin_vertex_id = (*it)[0].as<int>();
+        int tail_vertex_id = (*it)[1].as<int>();
+        std::string origin_vertex_name = (*vertex_id_name_map.find(origin_vertex_id)).second;
+        std::string tail_vertex_name = (*vertex_id_name_map.find(tail_vertex_id)).second;
         YAML::const_iterator bidirectional_info = (*it)[2]["bidirectional"].begin();
         bidirectional_info++;
         bool is_bidirectional = (*bidirectional_info).as<bool>();
+        graph.add_lane(origin_vertex_id, tail_vertex_id);
+        
         if (is_bidirectional)
         {
             std::cout << "Edge from " << origin_vertex_name << " to " << tail_vertex_name
                       << " is bidirectional." << std::endl;
+            graph.add_lane(tail_vertex_id, origin_vertex_id);
+            
         } else {
             std::cout << "Edge from " << origin_vertex_name << " to " << tail_vertex_name
                   << " not bidirectional." << std::endl;
         }
     }
+
+    // TODO: Add door functionality. Need to check how this is represented in traffic-editor
 
     return graph;
 }
